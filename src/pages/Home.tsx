b54bc2be2ab7e11669bffc77ef7a2f9faa2dee97
Santiago Jimenez <santiago.jimenez.casas@gmail.com>
2024-04-09 20:15:53 -0600

    Update homepage URL and change name in Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ const Home: React.FC = () => {
     return (
         <div className="pages">
             <h1>Welcome to My Portfolio</h1>
-            <p>Hi, I'm [Your Name]. I'm a [Your Profession] based in [Your Location].</p>
+            <p>Hi, I'm Santiago. I'm a [Your Profession] based in [Your Location].</p>
             <p>Here, you can find examples of my work and learn more about me.</p>
         </div>
     );
